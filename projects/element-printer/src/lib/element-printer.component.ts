6093c9e283f09ce9b3da1457578239e934c7433a
Soundwubz <soundwubz@gmail.com>
2019-03-11 16:55:12 -0400

    Nested and Angular style work began
    Nested classes initially working. Still working on an efficient way of acquiring all of the classes and getting all of the styles.  Next commit should have this working.

--- projects/element-printer/src/lib/element-printer.component.ts
+++ projects/element-printer/src/lib/element-printer.component.ts
@@ -21,38 +21,66 @@ export class ElementPrinterComponent implements OnInit {
   print() {
     const sudo = this;
     const container = this.renderer.selectRootElement('#' + this.containerToPrint, true);
+    const childElements = this.slice(container.children);
+    console.log('child elements:');
+    console.dir(childElements);
+    let classes: any[];
     console.dir(container);
-    this.slice(document.styleSheets).filter(filterSheets);
+    console.log('all elements:');
+    this.slice(document.all).forEach(x => {
+      if (container.contains(x)){ // gets each element within the container; use this to get the class names and styles
+        console.dir(x);
+        x.classList.forEach(y => {
+          classes.push(y); // can't do push on empty array
+        });
+      }
+    });
+    console.log('classes: ');
+    console.dir(classes);
     let styleText;
+    this.slice(document.styleSheets).filter(filterSheets);
 
     function filterSheets(item) {
       if (item.href === null) {
         const rules = sudo.slice(item.rules);
-        if (rules.length < 100) {
-          return rules.filter(filterRules);
-        }
+        console.log('rules: ');
+        console.dir(rules);
+        return true;
+        /* return rules.filter(filterRules); */
       } else {
         return false;
       }
     }
 
     function filterRules(item) {
+      console.dir(item);
       if (item.selectorText !== undefined) {
-        if (item.selectorText.includes(container.className)) {
-          console.log('class found: ' + item.selectorText);
-          styleText = item.cssText;
-          return true;
-        } else {
-          return false;
-        }
+        /* classes.forEach(x => {
+          if (item.selectorText.includes(x)) { // Go through classes and get styles;
+            if (styleText !== undefined) {
+              styleText = styleText + item.cssText;
+            } else {
+              styleText = item.cssText;
+            }
+            return true;
+          } else { // check if children match selector text
+            childElements.forEach(child => {
+              if (item.selectorText.includes(child.className)) { // Go through classes and get styles;
+                styleText = styleText + item.cssText;
+                return true;
+              } else {
+                return false;
+              }
+            });
+          }
+        }); */
       } else {
         return false;
       }
     }
 
     // Creating Print Window
-
-    const win = window.open('', 'Element Printer', 'height=700,width=700');
+    /* const win = window.open('', 'Element Printer', 'height=700,width=700');
     win.document.write('<html><head>');
     win.document.write('<title>Element Printer</title>');
     win.document.write('<style>');
@@ -63,7 +91,7 @@ export class ElementPrinterComponent implements OnInit {
     win.document.write(container.outerHTML);
     win.document.write('</body></html>');
     win.document.close();
-    win.print();
+    win.print(); */
 
   }
 
